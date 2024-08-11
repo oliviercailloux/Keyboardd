00f0360bbfd8a89bf06f14fa3d181961ec8a71d1
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-11 12:43:14 +0200

    Prerf pattern

--- src/main/java/io/github/oliviercailloux/keyboardd/mapping/XkbKeymapDecomposer.java
+++ src/main/java/io/github/oliviercailloux/keyboardd/mapping/XkbKeymapDecomposer.java
@@ -9,19 +9,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class XkbKeymapDecomposer {
+
   @SuppressWarnings("unused")
   private static final Logger LOGGER = LoggerFactory.getLogger(XkbKeymapDecomposer.class);
-  
-  private static final Pattern SYMBOLS = Pattern.compile("^xkb_symbols \"(?<name>.+)\" \\{[\\n\\r]*(?<contents>([^\\{]*\\{[^\\}]*\\})*[^\\}]+)[\\n" + //
-        "\\r]*\\};$", Pattern.MULTILINE | Pattern.DOTALL);
+
+  private static final String INTRO = "^xkb_symbols \"(?<name>[^\"]++)\" \\{";
+  private static final String SKIP = "[\\n\\r]*+";
+  private static final String INSIDE_BRACKETS = "([^\\{\\}]*+\\{[^\\}]*+\\})*+";
+  private static final Pattern SYMBOLS = Pattern.compile(
+      INTRO + SKIP + "(?<contents>" + INSIDE_BRACKETS + "[^\\{\\}]++)[\\n\\r]*+\\};$",
+      Pattern.MULTILINE | Pattern.DOTALL);
   private static final Pattern P_OTHER = Pattern.compile(
       "^(default )?(partial.*)|(xkb_symbols .*)|( *key.type.*)|( *name.*)|( *include .+)|( *modifier_map.*)|(\\};)$");
 
   public static ImmutableMap<String, String> bySymbolsMap(CharSource source) throws IOException {
-     Matcher matcher = SYMBOLS.matcher(source.read());
-     final ImmutableMap.Builder<String, String> builder = new ImmutableMap.Builder<>();
-     
-    while(matcher.find()) {
+    Matcher matcher = SYMBOLS.matcher(source.read());
+    final ImmutableMap.Builder<String, String> builder = new ImmutableMap.Builder<>();
+
+    while (matcher.find()) {
       String name = matcher.group("name");
       String symbols = matcher.group("contents");
       LOGGER.info("Found name: {}, symbols {}.", name, symbols.substring(0, 50));
