c4fc1df9c8cd56fd37c053477a5c89d222ee093b
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-13 19:49:03 +0200

    More The Key

--- src/test/java/io/github/oliviercailloux/keyboardd/the_key/TheKeyMapTests.java
+++ src/test/java/io/github/oliviercailloux/keyboardd/the_key/TheKeyMapTests.java
@@ -1,16 +1,12 @@
 package io.github.oliviercailloux.keyboardd.the_key;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.io.CharSource;
 import com.google.common.io.Resources;
 import io.github.oliviercailloux.jaris.io.PathUtils;
 import io.github.oliviercailloux.jaris.xml.DomHelper;
 import io.github.oliviercailloux.keyboardd.mapping.KeyboardMap;
-import io.github.oliviercailloux.keyboardd.mapping.KeysymEntry.Mnemonic;
 import io.github.oliviercailloux.keyboardd.mapping.XkbSymbolsReader;
 import io.github.oliviercailloux.keyboardd.mnemonics.CanonicalKeyboardMap;
 import io.github.oliviercailloux.keyboardd.mnemonics.CanonicalKeysymEntry;
@@ -22,8 +18,6 @@ import io.github.oliviercailloux.keyboardd.representable.XKeyNamesAndRepresenter
 import io.github.oliviercailloux.keyboardd.xkeys.Xkeys;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
 
@@ -42,7 +36,6 @@ public class TheKeyMapTests {
         XKeyNamesAndRepresenter.from(canonMap, XKeyNamesAndRepresenter::defaultRepresentation);
     Document outputDocument = inputSvg.withRepresentations(representer::representations);
     String outputString = DomHelper.domHelper().toString(outputDocument);
-    Files.writeString(Path.of("out.svg"), outputString);
 
     CharSource expectedOutput = Resources.asCharSource(
         getClass().getResource("The Key with default common+us representations.svg"), StandardCharsets.UTF_8);
@@ -61,10 +54,28 @@ public class TheKeyMapTests {
     XKeyNamesAndRepresenter representer = XKeyNamesAndRepresenter.from(canonMap, this::represent);
     Document outputDocument = inputSvg.withRepresentations(representer::representations);
     String outputString = DomHelper.domHelper().toString(outputDocument);
-    Files.writeString(Path.of("out.svg"), outputString);
 
     CharSource expectedOutput = Resources.asCharSource(
         getClass().getResource("The Key with tweaked common+us representations.svg"), StandardCharsets.UTF_8);
+    assertEquals(expectedOutput.read(), outputString);
+  }
+
+  @Test
+  public void writeMappedTweakedBigger() throws IOException {
+    Document inputDocument =
+        DomHelper.domHelper().asDocument(PathUtils.fromResource(getClass(), "The Key.svg"));
+    SvgKeyboard inputSvg = SvgKeyboard.using(inputDocument);
+
+    KeyboardMap map = XkbSymbolsReader.common().overwrite(XkbSymbolsReader.us());
+    CanonicalKeyboardMap canonMap = CanonicalKeyboardMap
+        .canonicalize(map.canonicalize(Xkeys.latest().canonicalByAlias()), Mnemonics.latest());
+    XKeyNamesAndRepresenter representer = XKeyNamesAndRepresenter.from(canonMap, this::represent);
+    inputSvg.setFontSize(20);
+    Document outputDocument = inputSvg.withRepresentations(representer::representations);
+    String outputString = DomHelper.domHelper().toString(outputDocument);
+
+    CharSource expectedOutput = Resources.asCharSource(
+        getClass().getResource("The Key with tweaked common+us representations bigger.svg"), StandardCharsets.UTF_8);
     assertEquals(expectedOutput.read(), outputString);
   }
 
--- src/test/java/io/github/oliviercailloux/keyboardd/the_key/TheKeyXKeyNamesTests.java
+++ src/test/java/io/github/oliviercailloux/keyboardd/the_key/TheKeyXKeyNamesTests.java
@@ -5,14 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.CharSource;
 import com.google.common.io.Resources;
-import io.github.oliviercailloux.jaris.io.CloseablePathFactory;
 import io.github.oliviercailloux.jaris.io.PathUtils;
 import io.github.oliviercailloux.jaris.xml.DomHelper;
-import io.github.oliviercailloux.keyboardd.keyboard.json.JsonRectangularKeyboardReader;
-import io.github.oliviercailloux.keyboardd.representable.RectangularKeyboard;
 import io.github.oliviercailloux.keyboardd.representable.Representation;
 import io.github.oliviercailloux.keyboardd.representable.SvgKeyboard;
-import io.github.oliviercailloux.svgb.PositiveSize;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.Test;
@@ -21,17 +17,42 @@ import org.w3c.dom.Document;
 public class TheKeyXKeyNamesTests {
   @Test
   public void writeWithXKeyNames() throws IOException {
-    Document inputDocument = DomHelper.domHelper().asDocument(PathUtils.fromResource(getClass(), "The Key.svg"));
+    Document inputDocument =
+        DomHelper.domHelper().asDocument(PathUtils.fromResource(getClass(), "The Key.svg"));
     SvgKeyboard inputSvg = SvgKeyboard.using(inputDocument);
-    Document outputDocument = inputSvg.withRepresentations(this::represent);
+    Document outputDocument = inputSvg.withRepresentations(this::representName);
     String outputString = DomHelper.domHelper().toString(outputDocument);
-    
+
     CharSource expectedOutput = Resources.asCharSource(
         getClass().getResource("The Key with X key names.svg"), StandardCharsets.UTF_8);
     assertEquals(expectedOutput.read(), outputString);
   }
 
-  private ImmutableList<Representation> represent(String xKeyName) {
+  private ImmutableList<Representation> representName(String xKeyName) {
     return ImmutableList.of(Representation.fromString(xKeyName));
+  }
+
+  @Test
+  public void writeWithChosenRepresentations() throws IOException {
+    Document inputDocument =
+        DomHelper.domHelper().asDocument(PathUtils.fromResource(getClass(), "The Key.svg"));
+    SvgKeyboard inputSvg = SvgKeyboard.using(inputDocument);
+    inputSvg.setFontSize(15);
+    Document outputDocument = inputSvg.withRepresentations(this::represent);
+    String outputString = DomHelper.domHelper().toString(outputDocument);
+    
+    CharSource expectedOutput = Resources.asCharSource(
+        getClass().getResource("The Key with chosen representations.svg"), StandardCharsets.UTF_8);
+    assertEquals(expectedOutput.read(), outputString);
+  }
+
+  private ImmutableList<Representation> represent(String xKeyName) {
+    final String repr = switch(xKeyName) {
+      case "LCTL" -> "Ctrl";
+      case "AB03" -> "C";
+      case "AB04" -> "V";
+      default -> xKeyName;
+    };
+    return ImmutableList.of(Representation.fromString(repr));
   }
 }
