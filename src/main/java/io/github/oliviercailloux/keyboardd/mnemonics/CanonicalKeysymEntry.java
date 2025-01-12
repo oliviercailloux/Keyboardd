96304595a9efcfed1425f5d577d003590d936c02
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-01-12 23:30:29 +0100

    probeUcp

--- src/main/java/io/github/oliviercailloux/keyboardd/mnemonics/CanonicalKeysymEntry.java
+++ src/main/java/io/github/oliviercailloux/keyboardd/mnemonics/CanonicalKeysymEntry.java
@@ -1,5 +1,13 @@
 package io.github.oliviercailloux.keyboardd.mnemonics;
 
+import java.util.Optional;
+
 public sealed interface CanonicalKeysymEntry permits CanonicalMnemonic, ImplicitUcp {
-  
+  default Optional<Integer> probeUcp() {
+    if (this instanceof ImplicitUcp ucp) {
+      return Optional.of(ucp.ucp());
+    }
+    CanonicalMnemonic mnemonic = (CanonicalMnemonic) this;
+    return mnemonic.ucp();
+  }
 }
